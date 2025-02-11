e07a5e990872ca8fe16a7602e5a15b927d0708fb
Pooja <pooja.pimpalshende215@gmail.com>
2025-02-11 17:00:17 +0100

    Implement active section tracking and improve header navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,33 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState("home");
   return (
     <>
-      <Header />
-      <Element name="home">
-        <Home />
-      </Element>
-      <Element name="about">
-        <About />
-      </Element>
-      <Element name="experience">
-        <Experience />
-      </Element>
-      <Element name="contact">
-        <Contact />
-      </Element>
+      <Header setActive={setActive} />
+      {active === "home" && (
+        <Element name="home">
+          <Home />
+        </Element>
+      )}
+      {active === "about" && (
+        <Element name="about">
+          <About />
+        </Element>
+      )}
+      {active === "experience" && (
+        <Element name="experience">
+          <Experience />
+        </Element>
+      )}
+      {active === "contact" && (
+        <Element name="contact">
+          <Contact />
+        </Element>
+      )}
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,43 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-scroll";
 import styles from "../Header/Header.module.css";
 
-const Header = () => {
+const Header = ({ setActive }) => {
   return (
     <div className={styles.header}>
-      <Link className={styles.link} to="home" smooth={true}>
+      <Link
+        className={styles.link}
+        to="home"
+        smooth={true}
+        offset={-80}
+        onClick={() => setActive("home")}
+      >
         Home
       </Link>
-      <Link className={styles.link} to="about" smooth={true}>
+      <Link
+        className={styles.link}
+        to="about"
+        smooth={true}
+        offset={-100}
+        onClick={() => setActive("about")}
+      >
         About
       </Link>
-      <Link className={styles.link} to="experience" smooth={true}>
+      <Link
+        className={styles.link}
+        to="experience"
+        smooth={true}
+        offset={-100}
+        onClick={() => setActive("experience")}
+      >
         Experience
       </Link>
-      <Link className={styles.link} to="contact" smooth={true}>
+      <Link
+        className={styles.link}
+        to="contact"
+        smooth={true}
+        onClick={() => setActive("contact")}
+      >
         Contact
       </Link>
     </div>
